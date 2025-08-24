6a9bea024502311a93bedd0ee9253d3ef04a7577
Abigail10102012 <kavdb35@gmail.com>
2025-08-24 10:17:27 +0200

    Add files via upload

--- face.py
+++ face.py
@@ -1,6 +1,6 @@
-import cv2 
+import cv2
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_defult.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -17,7 +17,12 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiscale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(
+    gray, 
+    scaleFactor=1.1, 
+    minNeighbors=5, 
+    minSize=(30, 30)
+)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
